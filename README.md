e4387ed1b21b710c334c96a6b6133332e623bd9e
Noel <cutie@floofy.dev>
2024-10-01 19:01:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🐻‍❄️🍯 `noeldoc-build/protobufs`
+This repository contains all the Protocol Buffers schemas for Noeldoc systems. For now, this hosts the Extension Protocol and Runner protobufs.
+
+## License
+**noeldoc-build/build-protobufs** is released under the **Apache 2.0 License**. Please refer to the [LICENSE](/LICENSE) file for more information.
---
+++ flake.nix
@@ -0,0 +1,44 @@
+# 🐻‍❄️🍯 noeldoc/protobufs: Contains the Protocol Buffers files for Noeldoc systems
+# Copyright 2024 Noelware, LLC. <team@noelware.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+{
+  description = "🐻‍❄️🍯 Contains the Protocol Buffers files for Noeldoc systems";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    systems,
+    ...
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    nixpkgsFor = system:
+      import nixpkgs {
+        inherit system;
+      };
+  in {
+    formatter = forEachSystem (system: (nixpkgsFor system).alejandra);
+    devShells = forEachSystem (system: let
+      pkgs = nixpkgsFor system;
+    in {
+      default = import ./nix/devshell.nix {inherit pkgs;};
+    });
+  };
+}
